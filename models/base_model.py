fe85987873d78dac3e30c0313cf6f1269a48f28a
Cu7ious <328@holbertonschool.com>
2018-06-18 03:35:14 +0000

    Solution for the task #3

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """
 from uuid import uuid4
 from datetime import datetime
+import json
 
 
 class BaseModel:
@@ -12,21 +13,26 @@ class BaseModel:
         """ Constructor of the BaseModel class
         """
         self.id = str(uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
-        print(self.id, self.created_at, self.updated_at)
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        print("[BaseModel] ({0}) {1}".format(self.id, self.__dict__))
+        """ Human readable representation of the instance
+        """
+        cls_name = __class__.__name__
+        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
 
     def save(self):
         """ Saves the updates of the state of model
         """
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__
         """
-        pass
+        raw_dict = self.__dict__
+        raw_dict['__class__'] = __class__.__name__
+        raw_dict['created_at'] = self.created_at.isoformat()
+        raw_dict['updated_at'] = self.updated_at.isoformat()
 
-BaseModel()
+        return raw_dict
